1a133d0fdb4084374a72a95f0d29551ac11d0ed6
Dejan <vujkovdejan@gmail.com>
2024-02-05 10:56:31 +0100

    change to support stow

--- nvim/lua/custom/plugins/alpha.lua
+++ nvim/lua/custom/plugins/alpha.lua
@@ -25,7 +25,8 @@ return {
       dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
       dashboard.button("f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
       dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-      dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+      dashboard.button("s", "  > Settings",
+        ":e $HOME/.dotfiles/.config/nvim/init.lua | :cd %:p:h | wincmd k | pwd<CR>"),
       dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
     }
     -- Send config to alpha
