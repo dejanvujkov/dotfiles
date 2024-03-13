5dd860ba74fd94ac75a589cd31cb30131f9f4f56
Dejan <vujkovdejan@gmail.com>
2024-03-13 07:58:50 +0100

    see current variable in debug keymap

--- .config/nvim/lua/kickstart/plugins/debug.lua
+++ .config/nvim/lua/kickstart/plugins/debug.lua
@@ -40,6 +40,7 @@ return {
     vim.keymap.set('n', '<F11>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<F10>', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<F9>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<F8>', dapui.eval, { desc = 'Debug: See Current Variable' })
     vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
