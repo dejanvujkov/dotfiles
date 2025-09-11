f75028cbeb06742c6896f8a396225991c4cc1782
Dejan Vujkov <vujkovdejan@gmail.com>
2025-09-11 14:47:37 +0200

    ai stuff

--- .config/nvim/lua/custom/plugins/ai.lua
+++ .config/nvim/lua/custom/plugins/ai.lua
@@ -83,7 +83,7 @@ return {
         auto_trigger = true,
         hide_during_completion = vim.g.ai_cmp,
         keymap = {
-          accept = false, -- handled by nvim-cmp / blink.cmp
+          accept = '<Tab>',
           next = '<M-]>',
           prev = '<M-[>',
         },
